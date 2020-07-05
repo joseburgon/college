45567453415c9f228851063ef0abb78116c70b78
Jose Burgon <burgonjose@gmail.com>
2020-07-05 13:47:58 -0500

    Register and Response views designed

--- resources/js/views/Register.vue
+++ resources/js/views/Register.vue
@@ -1,121 +1,194 @@
 <template>
-    <div class="flex">
-        <FormulateForm
-            class="w-full max-w-lg px-6"
-            v-model="formValues"
-            id="registerForm"
-            action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/"
-            method="post"
-            @submit="addStudent"
-        >
-            <h2 class="font-normal text-black text-5xl text-center mb-8">
-                Registrarme
-            </h2>
+    <div class="flex-col">
+        <div class="flex flex-wrap md:6 lg:px-20">
+            <div class="flex w-full md:w-1/2">
+                <FormulateForm
+                    class="w-full max-w-lg px-2 md:px-8"
+                    v-model="formValues"
+                    id="registerForm"
+                    action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/"
+                    method="post"
+                    @submit="addStudent"
+                >
+                    <h2
+                        class="font-normal text-black text-2xl md:text-4xl mb-4 md:mb-8"
+                    >
+                        Registrarme
+                    </h2>
+                    <FormulateInput
+                        type="hidden"
+                        name="ApiKey"
+                        value="4Vj8eK4rloUd272L48hsrarnUA"
+                    />
+                    <FormulateInput
+                        name="merchantId"
+                        type="hidden"
+                        value="508029"
+                    />
+                    <FormulateInput
+                        name="referenceCode"
+                        type="hidden"
+                        value=""
+                    />
+                    <FormulateInput name="amount" type="hidden" value="20000" />
+                    <FormulateInput
+                        name="accountId"
+                        type="hidden"
+                        value="512321"
+                    />
+                    <FormulateInput
+                        name="description"
+                        type="hidden"
+                        value="Curso de Finanzas"
+                    />
+                    <FormulateInput name="extra1" type="hidden" value="" />
+                    <FormulateInput name="extra2" type="hidden" value="2" />
+                    <FormulateInput name="currency" type="hidden" value="COP" />
+                    <FormulateInput name="signature" type="hidden" value="" />
+                    <FormulateInput
+                        name="buyerFullName"
+                        type="hidden"
+                        value=""
+                    />
+                    <FormulateInput name="test" type="hidden" value="1" />
+                    <FormulateInput
+                        name="responseUrl"
+                        type="hidden"
+                        value="http://college.test/response"
+                    />
+                    <FormulateInput
+                        name="confirmationUrl"
+                        type="hidden"
+                        value="http://college.test/api/transactions"
+                    />
+                    <div class="grid grid-flow-col grid-cols-2 gap-4">
+                        <FormulateInput
+                            name="name"
+                            type="text"
+                            label="Tu nombre"
+                            placeholder="Tu primer nombre"
+                            validation="required"
+                            :validation-messages="{
+                                required: 'Nombre es requerido'
+                            }"
+                            outer-class="formulate-input flex-grow pr-2"
+                            label-class="text-xs font-bold"
+                        />
+                        <FormulateInput
+                            name="last_name"
+                            type="text"
+                            label="Tu apellido"
+                            placeholder="Tu apellido"
+                            validation="required"
+                            :validation-messages="{
+                                required: 'Apellido es requerido'
+                            }"
+                            outer-class="formulate-input flex-grow pl-2"
+                            label-class="text-xs font-bold"
+                        />
+                    </div>
 
-            <FormulateInput
-                type="hidden"
-                name="ApiKey"
-                value="4Vj8eK4rloUd272L48hsrarnUA"
-            />
-            <FormulateInput name="merchantId" type="hidden" value="508029" />
-            <FormulateInput name="referenceCode" type="hidden" value="" />
-            <FormulateInput name="amount" type="hidden" value="20000" />
-            <FormulateInput name="accountId" type="hidden" value="512321" />
-            <FormulateInput
-                name="description"
-                type="hidden"
-                value="Curso de Finanzas"
-            />
-            <FormulateInput name="extra1" type="hidden" value="" />
-            <FormulateInput name="extra2" type="hidden" value="2" />
-            <FormulateInput name="currency" type="hidden" value="COP" />
-            <FormulateInput name="signature" type="hidden" value="" />
-            <FormulateInput name="buyerFullName" type="hidden" value="" />
-            <FormulateInput name="test" type="hidden" value="1" />
-            <FormulateInput
-                name="responseUrl"
-                type="hidden"
-                value="http://college.test/response"
-            />
-            <FormulateInput
-                name="confirmationUrl"
-                type="hidden"
-                value="http://college.test/api/transactions"
-            />
-            <div class="grid grid-flow-col grid-cols-2 gap-4">
-                <FormulateInput
-                    name="name"
-                    type="text"
-                    label="Tu nombre"
-                    placeholder="Tu primer nombre"
-                    validation="required"
-                    outer-class="formulate-input flex-grow pr-2"
-                    label-class="text-xs font-bold"
-                />
-                <FormulateInput
-                    name="last_name"
-                    type="text"
-                    label="Tu apellido"
-                    placeholder="Tu apellido"
-                    validation="required"
-                    outer-class="formulate-input flex-grow pl-2"
-                    label-class="text-xs font-bold"
-                />
-            </div>
+                    <div class="flex-col justify-center pb-6">
+                        <FormulateInput
+                            name="buyerEmail"
+                            type="email"
+                            label="Correo electrónico"
+                            placeholder="Email"
+                            validation="required|email"
+                            :validation-messages="{
+                                required: 'Tu correo es requerido',
+                                email: 'Tu correo debe ser un email válido'
+                            }"
+                            element-class="flex-grow"
+                            label-class="text-xs font-bold"
+                        />
+                    </div>
 
-            <div class="flex-col justify-center pb-6">
-                <FormulateInput
-                    name="buyerEmail"
-                    type="email"
-                    label="Correo electrónico"
-                    placeholder="Email"
-                    validation="required|email"
-                    element-class="flex-grow"
-                    label-class="text-xs font-bold"
-                />
+                    <div class="grid grid-flow-col grid-cols-2 gap-4">
+                        <FormulateInput
+                            name="identification"
+                            type="text"
+                            label="Número de cédula"
+                            placeholder="Tu cédula"
+                            validation="required"
+                            :validation-messages="{
+                                required: 'Cedula es requerida'
+                            }"
+                            element-class="flex justify-center"
+                            outer-class="formulate-input flex-grow pr-2"
+                            label-class="text-xs font-bold"
+                        />
+                        <FormulateInput
+                            name="mobilePhone"
+                            type="text"
+                            label="Número de teléfono"
+                            placeholder="Tu teléfono"
+                            validation="required|number"
+                            :validation-messages="{
+                                required: 'Tu teléfono es requerido',
+                                number: 'Tu teléfono debe ser un número'
+                            }"
+                            outer-class="formulate-input flex-grow pl-2"
+                            label-class="text-xs font-bold"
+                        />
+                    </div>
+                    <div class="flex-col justify-center mb-6">
+                        <FormulateInput
+                            name="billingCity"
+                            type="text"
+                            label="Ciudad de residencia"
+                            placeholder="Tu ciudad"
+                            validation="required"
+                            :validation-messages="{
+                                required: 'Ciudad es requerida'
+                            }"
+                            element-class="flex-grow"
+                            label-class="text-xs font-bold"
+                        />
+                    </div>
+                    <div class="div mt-10">
+                        <FormulateInput
+                            type="submit"
+                            name="Registrarme y Pagar"
+                        />
+                        <!-- <pre class="code px-2" v-text="formValues" /> -->
+                    </div>
+                </FormulateForm>
             </div>
-
-            <div class="grid grid-flow-col grid-cols-2 gap-4">
-                <FormulateInput
-                    name="identification"
-                    type="text"
-                    label="Número de cédula"
-                    placeholder="Tu cédula"
-                    validation="required"
-                    element-class="flex justify-center"
-                    outer-class="formulate-input flex-grow pr-2"
-                    label-class="text-xs font-bold"
-                />
-                <FormulateInput
-                    name="mobilePhone"
-                    type="text"
-                    label="Número de teléfono"
-                    placeholder="Tu teléfono"
-                    validation="required|number"
-                    outer-class="formulate-input flex-grow pl-2"
-                    label-class="text-xs font-bold"
-                />
+            <div
+                class="bg-gray-100 rounded w-full md:w-1/2 mt-6 md:mt-0 sm:mt-6"
+            >
+                <div class="md:flex py-4 px-8">
+                    <div class="flex-col flex-wrap mt-4 md:mt-0">
+                        <div
+                            class="uppercase tracking-wide text-sm text-dustyGray font-bold my-4"
+                        >
+                            Curso de finanzas
+                        </div>
+                        <a
+                            href="#"
+                            class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+                            >De la carencia a la abundancia</a
+                        >
+                        <p class="mt-2 text-gray-600">
+                            Getting a new business off the ground is a lot of
+                            hard work. Here are five ideas you can use to find
+                            your first customers.
+                        </p>
+                        <hr class="border-gray-400 my-4 lg:my-8" />
+                        <h3 class="text-2xl font-bold mb-4">$80.000 COP</h3>
+                        <px-payment-methods />
+                    </div>
+                </div>
             </div>
-            <div class="flex-col justify-center mb-6">
-                <FormulateInput
-                    name="billingCity"
-                    type="text"
-                    label="Ciudad de residencia"
-                    placeholder="Tu ciudad"
-                    validation="required"
-                    element-class="flex-grow"
-                    label-class="text-xs font-bold"
-                />
-            </div>
-            <FormulateInput type="submit" name="Registrarme y Pagar" />
-            <pre class="code px-2" v-text="formValues" />
-        </FormulateForm>
+        </div>
     </div>
 </template>
 
 <script>
 import md5 from "crypto-js/md5";
 import "../../sass/form/form.scss";
+import PxPaymentMethods from "../components/PxPaymentMethods";
 
 export default {
     data() {
@@ -126,6 +199,7 @@ export default {
             cedula: ""
         };
     },
+    components: { PxPaymentMethods },
     methods: {
         addStudent() {
             this.formValues.extra1 = this.formValues.identification;
--- resources/js/views/Response.vue
+++ resources/js/views/Response.vue
@@ -1,50 +1,68 @@
 <template>
-    <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="flex justify-center mt-16">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg py-8">
             <div class="flex justify-center mt-8">
-                <px-thumbs-up class="w-12" />
+                <px-thumbs-up class="w-12" v-if="transactionState == 4" />
+                <px-thumbs-down class="w-12" v-else />
             </div>
             <div class="flex flex-col justify-center items-center px-6 py-4">
-                <p class="font-light text-sm">Hey Juan Carlos</p>
+                <p class="font-light text-sm">Resultado del pago:</p>
                 <p class="font-bold text-2xl mb-2">
-                    ¡Transacción Aprobada!
+                    {{ estadoTx }}
                 </p>
-                <p class="text-gray-700 text-base text-center">
-                    Con esto has quedado matriculado para el:
+                <p
+                    class="text-gray-700 text-base text-center"
+                    v-if="transactionState == 4"
+                >
+                    Con esto has quedado matriculado para el curso:
                     <span class="tx-wildsand font-semibold">
-                        Curso de Finanzas.
+                        {{ query.description }}
                     </span>
-                    Enviaremos tu usuario y contraseña a tu mail para que puedas
-                    acceder a la plataforma tan pronto se lance el curso.
+                    Enviaremos tu usuario y contraseña a tu mail ({{
+                        query.buyerEmail
+                    }}) para que puedas acceder a la plataforma tan pronto se
+                    lance el curso.
                 </p>
-            </div>
-            <div class="flex justify-center px-6 py-4">
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-                    >#photography</span
-                >
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-                    >#travel</span
-                >
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-                    >#winter</span
-                >
+                <p class="text-gray-700 text-base text-center" v-else>
+                    {{ query.message }}
+                </p>
             </div>
         </div>
-    </section>
+    </div>
 </template>
 
 <script>
 import PxThumbsUp from "../components/PxThumbsUp";
+import PxThumbsDown from "../components/PxThumbsDown";
 export default {
     data() {
         return {
             query: this.$route.query,
-            Students: []
+            transactionState: "",
+            estadoTx: ""
         };
     },
-    components: { PxThumbsUp }
+    components: { PxThumbsUp, PxThumbsDown },
+    created() {
+        this.transactionState = this.query.transactionState;
+
+        switch (parseInt(this.transactionState)) {
+            case 4:
+                this.estadoTx = "Transacción Aprobada";
+                break;
+            case 6:
+                this.estadoTx = "Transacción Rechazada";
+                break;
+            case 7:
+                this.estadoTx = "Transacción Pendiente";
+                break;
+            case 104:
+                this.estadoTx = "Error";
+                break;
+            default:
+                this.estadoTx = this.query.message;
+        }
+        console.log(this.estadoTx);
+    }
 };
 </script>
