ce69ea7026b2e36e2381d59d442a506bf2351541
Jose Burgon <burgonjose@gmail.com>
2020-08-05 21:28:26 -0500

    Implementing Mercado Pago

--- app/Http/Controllers/Api/ReferenceCodeController.php
+++ app/Http/Controllers/Api/ReferenceCodeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use App\Models\ReferenceCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use MercadoPago;
 
 class ReferenceCodeController extends Controller
 {
@@ -20,10 +22,38 @@ class ReferenceCodeController extends Controller
 
     public function store(Request $request)
     {
+        $course = Course::find($request->course);
+
+        // Agrega credenciales
+        MercadoPago\SDK::setAccessToken(env('MERCADOPAGO_ACCESS_TOKEN'));
+
+        // Crea un objeto de preferencia
+        $preference = new MercadoPago\Preference();
+
+        // Crea un ítem en la preferencia
+        $item = new MercadoPago\Item();
+
+        $preference->back_urls = array(
+            "success" => "http://checkout.livingroomcollege.org/response",
+            "failure" => "http://checkout.livingroomcollege.org/response",
+            "pending" => "http://checkout.livingroomcollege.org/response"
+        );
+        
+        $preference->auto_return = "approved";
+
+        $preference->notification_url = 'http://checkout.livingroomcollege.org/api/notifications?source_news=ipn';
+
+        $item->title = $course->name;
+        $item->quantity = 1;
+        $item->unit_price = $course->price;
+        $preference->items = array($item);
+        $preference->save();
+
         $referenceCode = new ReferenceCode;
-        $referenceCode->code = $request->prefix . '-' . Str::random(8);
+        $referenceCode->code = $preference->id;
         $referenceCode->course_id = $request->course;
         $referenceCode->save();
+
         return $referenceCode->toJson();
     }
 }
