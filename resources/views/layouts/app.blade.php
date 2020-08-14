9ce07ae1d69252a1906e134dca3d179f31a68f18
Jose Burgon <burgonjose@gmail.com>
2020-08-14 17:49:01 -0500

    Implementing Montserrat font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     theme: {
         fontFamily: {
             sans: [
+                '"Montserrat"',
                 '"Helvetica"',
                 "-apple-system",
             ],
