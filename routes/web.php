04152353458440115fcad1b7039e56e54d2c83a7
Jose Burgon <burgonjose@gmail.com>
2020-08-16 16:00:20 -0500

    Cleaning web routes file

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
-use App\Mail\TestEmail;
-use App\Mail\ThinkificCredentials;
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Mail;
-
-Route::get('resend', function () {
-    $userData = [
-        'first_name' => 'Testing',
-        'email' => 'testing@yahoo.com',
-        'password' => 'OElYuuQA',
-    ];
-
-    Mail::to('team@livingroomcollege.org')->send(new ThinkificCredentials($userData));
-    return '<h1>Enviado!</h1>';
-});
 
 Auth::routes();
 
