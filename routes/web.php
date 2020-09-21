a0d2d284ab8e370d00bb2f6d6da42c2fdf8368f3
Jose Burgon <burgonjose@gmail.com>
2020-09-21 14:01:19 -0500

    Installing Sentry

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug-sentry', function () {
+    throw new Exception('Testing Sentry errors!');
+});
+
 Route::get('{any}', 'AppController@index')
     ->where('any', '.*')
     ->name('home');
