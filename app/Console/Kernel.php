8e084610bdc6da5edb06c17f6876fe8605436816
Jose Burgon <burgonjose@gmail.com>
2020-10-23 17:28:02 -0500

    Implementing Laravel Excel exports

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(LoggingTest::class)->everyMinute();
-
         $schedule->command(UpdateEnrollments::class)->dailyAt('07:30');
     }
 
