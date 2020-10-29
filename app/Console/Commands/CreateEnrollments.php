21d76776833890e1d6567971bc141d586be4eba7
Jose Burgon <burgonjose@gmail.com>
2020-10-29 12:15:35 -0500

    Improving students import and creating masive enrollments command

---
+++ app/Console/Commands/CreateEnrollments.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Course;
+use App\Models\Student;
+use App\Repositories\ThinkificApi;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class CreateEnrollments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'enrollments:create {course}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creating masive enrollments for students with imported status';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Student::where('status', 'imported')->chunk(50, function ($student) {
+
+            $this->info('Enrolling student: ' . $student->email);
+
+            $user = $this->userCreationProccess($student);
+
+            Log::info('Thinkific user created with ID: ' . $user['id']);
+
+            $student->fill([
+                'thinkific_user_id' => $user['id'],
+                'status' => 'user created'
+            ])->save();
+
+            $course = Course::find($this->argument('course'));
+
+            $this->enrollmentProccess($user, $course, $student);
+
+            $this->info('Student: ' . $student->email . ' enrolled.');
+
+        });
+    }
+
+    private function userCreationProccess($student)
+    {
+
+        $apiRepo = new ThinkificApi();
+
+        $userExists = $apiRepo->checkIfUserExists($student->email);
+
+        Log::info('User exists?', ['result' => $userExists]);
+
+        if ($userExists) {
+
+            Log: info('User already exists');
+
+            $user = $apiRepo->getUser($student->email);
+
+            return $user;
+        }
+
+        $password = 'College*2020';
+
+        $userData = [
+            'email' => $student->email,
+            'first_name' => $student->name,
+            'last_name' => $student->last_name,
+            'password' => $password,
+            'send_welcome_email' => false
+        ];
+
+        $user = $apiRepo->createUser($userData);
+
+        return $user;
+    }
+
+    private function enrollmentProccess($user, $course, $student)
+    {
+        $apiRepo = new ThinkificApi();
+
+        $enrollmentData = [
+            'course_id' => $course->thinkific_id,
+            'user_id' => $user['id'],
+            'activated_at' => $course->available_at,
+        ];
+
+        $enrollment = $apiRepo->createEnrollment($enrollmentData);
+
+        Log::info('Student enrolled successfully in course.', $enrollment);
+
+        $student->courses()->attach($course->id);
+
+        $student->fill(['status' => 'enrolled'])->save();
+
+    }
+}
--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -3,28 +3,57 @@
 namespace App\Imports;
 
 use App\Models\Student;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class StudentsImport implements ToModel
+class StudentsImport implements ToModel, WithHeadingRow
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-        return new Student([
-            'name' => $row[0],
-            'last_name' => $row[1],
-            'identification' => $row[2],
-            'phone' => $row[3],
-            'email' => $row[4],
-            'city' => $row[5],
-            'state' => $row[6],
-            'country' => $row[7],
-            'status' => $row[8],
-            'thinkific_user_id' => $row[9],
-        ]);
+        if ($this->studentExists($row)) {
+
+            return null;
+
+        } else {
+
+            return new Student([
+                'name' => $row['name'],
+                'last_name' => $row['last_name'],
+                'identification' => $row['identification'],
+                'phone' => $row['phone'],
+                'email' => $row['email'],
+                'city' => $row['city'],
+                'state' => $row['state'],
+                'country' => $row['country'],
+                'status' => $row['status'],
+                'thinkific_user_id' => $row['thinkific_user_id'],
+            ]);
+
+        }
+    }
+
+    private function studentExists($studentRow)
+    {
+        $student = Student::firstWhere('email', $studentRow['email']);
+
+        if ($student <> null) {
+
+            Log::info('Imported student found.', (array) $student);
+
+            $student->update($studentRow);
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
     }
 }
