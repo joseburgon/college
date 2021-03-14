cf9abf18186c1cc879a3da9da38272289d496526
Jose Burgon <burgonjose@gmail.com>
2021-03-14 10:54:03 -0300

    Adding facebook pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/main.js') }}" defer></script>
-    <script src="{{ 'https://maps.googleapis.com/maps/api/js?libraries=places&key='.config('app.google_api_key') }}"></script>
+    <script
+        src="{{ 'https://maps.googleapis.com/maps/api/js?libraries=places&key='.config('app.google_api_key') }}"></script>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
@@ -29,26 +30,61 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/form/form.css') }}" rel="stylesheet">
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '709663079657166');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=709663079657166&ev=PageView&noscript=1"
+        />
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+
     @yield('styles')
 </head>
 
 <body>
-    <div id="app" class="container mx-auto">
-        @yield('content')
-    </div>
+<div id="app" class="container mx-auto">
+    @yield('content')
+</div>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-E90CHSGC37"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E90CHSGC37"></script>
 
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
+<script>
+    window.dataLayer = window.dataLayer || [];
 
-        gtag('config', 'G-E90CHSGC37');
-    </script>
+    function gtag() {
+        dataLayer.push(arguments);
+    }
 
-    @yield('scripts')
+    gtag('js', new Date());
+
+    gtag('config', 'G-E90CHSGC37');
+</script>
+
+@yield('scripts')
 </body>
 
 </html>
